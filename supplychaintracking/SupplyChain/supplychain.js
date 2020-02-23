0e99b34f7b399c9df6b076aa320f46111af2c777
Zain Munir <zainmunir4112@gmail.com>
2020-02-23 15:23:37 +0500

    Add new block error handling

--- supplychaintracking/SupplyChain/supplychain.js
+++ supplychaintracking/SupplyChain/supplychain.js
@@ -11,13 +11,13 @@ var hash = require('object-hash');
 let blockchain;
 
 router.post('/add/supplier/:supplierId', async (req, res, next) => {
-    try{
+    try {
         const genesisBlock = { ...GENESIS_DATA };
         const getHash = hash(req.body.blocks);
         const lastHash = genesisBlock.hash;
         const blockData = { ...req.body.blocks, hash: getHash, lastHash: lastHash, timestamp: new Date() };
-        const blockArray = [ genesisBlock, blockData]; 
-        const reqBody = { ...req.body, supplierId: req.params.supplierId, timestamp: new Date(), blocks: blockArray};
+        const blockArray = [genesisBlock, blockData];
+        const reqBody = { ...req.body, supplierId: req.params.supplierId, blocks: blockArray };
 
         const supplyChain = new SupplyChainModel(reqBody);
         const supplierResponse = await supplyChain.save();
@@ -26,40 +26,47 @@ router.post('/add/supplier/:supplierId', async (req, res, next) => {
             data: supplierResponse,
             message: "Supplier block added successfully"
         })
-    }catch(error){
+    } catch (error) {
         let errorDoc = errorHandler(error);
         return res.status(errorDoc.status).send(errorDoc);
     }
 
-   
+
 })
 
 router.post('/add/:supplierId', async (req, res, next) => {
-    try{
+    try {
         const getData = await SupplyChainModel.findOne({ supplierId: req.params.supplierId });
 
+        if (!getData) {
+            throw {
+                status: 400,
+                message: "No supplier found"
+            }
+
+        }
         const length = getData.blocks.length;
-        const lastBlock = getData.blocks[length-1];
+        const lastBlock = getData.blocks[length - 1];
 
         const getHash = hash(req.body);
         const lastHash = lastBlock.hash;
-        const newBlock = { ...req.body, hash: getHash, lastHash: lastHash, timestamp: new Date()};
-        const blockArray = [ ...getData.blocks, newBlock];
+        const newBlock = { ...req.body, hash: getHash, lastHash: lastHash, timestamp: new Date() };
+        const blockArray = [...getData.blocks, newBlock];
 
-        const updateChain = await SupplyChainModel.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(getData._id)},
-            { $set: { blocks: blockArray}},
+        const updateChain = await SupplyChainModel.findByIdAndUpdate({ _id: mongoose.Types.ObjectId(getData._id) },
+            { $set: { blocks: blockArray } },
             { new: true }
         )
         return res.status(200).send({
             data: updateChain,
             message: "Block added successfully"
         })
-    }catch(error){
+    } catch (error) {
         let errorDoc = errorHandler(error);
         return res.status(errorDoc.status).send(errorDoc);
     }
 
- 
+
 })
 
 router.get('/delete_chain', async (req, res, next) => {
